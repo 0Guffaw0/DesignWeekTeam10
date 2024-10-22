455d74971da47499c1ac09aa7bf7975cdbffd116
Isaac <isaac.ebert@mohawkcollege.ca>
2024-10-22 15:33:23 -0400

    Change background properties

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -8,14 +8,19 @@ using UnityEngine;
 public class Background : MonoBehaviour
 {
     public float speed;
+    public bool isActive;
 
     [SerializeField]
     private Renderer bgRenderer;
 
+    public SpriteRenderer backgroundSpriteRenderer;
+    public Sprite[] backgroundSprites;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -23,14 +28,23 @@ public class Background : MonoBehaviour
     {
         bgRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
 
-        if (Input.GetKeyUp(KeyCode.A))
+        // Speed adjustment with stopping
+        if (Input.GetKeyDown(KeyCode.A))
         {
             speed += 0.1f;
         }
-        else if (Input.GetKeyUp(KeyCode.S) && !(speed <= 0))
+        else if (Input.GetKeyDown(KeyCode.S) && !(speed <= 0))
         {
             speed -= 0.1f;
         }
+
+        // Material changer for the background
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            isActive = !isActive;
+        }
+
+        bgRenderer.enabled = isActive;
     }
 
 }
