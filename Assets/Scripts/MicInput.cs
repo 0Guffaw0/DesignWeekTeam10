a297b680b5234a5cf233110d641507240401bbe6
Ethan K <ekoocher1616@gmail.com>
2024-10-22 13:43:03 -0400

    Update script

--- Assets/Scripts/MicInput.cs
+++ Assets/Scripts/MicInput.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 
 public class MicInput : MonoBehaviour
@@ -11,12 +12,21 @@ public class MicInput : MonoBehaviour
     public float speedMultiplier = 5f; //How fast to go when speaking/yelling
 
     private Rigidbody2D rb; //Store rigidbody2d
+    private AudioSource audioSource;
+    private Microphone mic;
 
     // Start is called before the first frame update
     void Start()
     {
         //Get character rigidbody2d
         rb = GetComponent<Rigidbody2D>();
+
+        audioSource = GetComponent<AudioSource>();
+
+        foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
 
         StartMic();
     }
@@ -26,14 +36,17 @@ public class MicInput : MonoBehaviour
 
     }
 
-    float GetLoudness()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+            audioSource.clip = Microphone.Start("Microphone (Intel® Smart Sound Technology for Digital Microphones)", true, 10, 44100);
+        }
 
+        if (Microphone.IsRecording("Microphone (Intel® Smart Sound Technology for Digital Microphones)"))
+        {
+            Debug.Log("AHHHH");
+        }
     }
 }
