c3134da7a6362f3553c6a203a26d66caecafec4d
Ethan K <ekoocher1616@gmail.com>
2024-10-24 23:29:24 -0400

    Update main

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.PackageManager.UI;
+
 using UnityEngine;
 
 public class Background : MonoBehaviour
